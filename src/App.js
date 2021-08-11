69aa334201b90cffbcc8556a0cf88541a13be8a5
rahul shukla <rahul.shukla754@gmail.com>
2021-08-11 22:54:04 +0530

    docs :  show all element

--- src/App.js
+++ src/App.js
@@ -1,63 +1,63 @@
 import { useState } from "react";
 import "./styles.css";
 
-export default function App() {
-  var emojiDictionary = {
-    "🍇": "Grapes",
-    "🍈": "Melon",
-    "🍉": "Watermelon",
-    "🍊": "Tangerine",
-    "🍋": "Lemon",
-    "🍌": "Banana",
-    "🍍": "Pineapple",
-    "🥭": "Mango",
-    "🍎": "Red Apple",
-    "🍐": "Pear",
-    "🍒": "Cherries",
-    "🍅": "Tomato",
-    "🍆": "Eggplant",
-    "🥕": "Carrot",
-    "🥦": "Broccoli",
-    "🍞": "Bread",
-    "🍗": "Poultry Leg",
-    "🥩": "Cut of Meat",
-    "🥓": "Bacon",
-    "🍔": "Hamburger",
-    "🍟": "French Fries",
-    "🍕": "Pizza",
-    "🌭": "Hot Dog",
-    "🥪": "Sandwich",
-    "🌮": "Taco",
-    "🥚": "Egg",
-    "🍿": "Popcorn",
-    "🧈": "Butter",
-    "🍨": "Ice Cream",
-    "🍩": "Doughnut",
-    "🍪": "Cookie",
-    "🎂": "Birthday Cake",
-    "🍰": "Shortcake",
-    "🧁": "Cupcake",
-    "🥧": "Pie",
-    "🍫": "Chocolate Bar",
-    "🍬": "Candy",
-    "🍭": "Lollipop",
-    "🍶": "Sake",
-    "🍾": "Bottle with Popping Cork",
-    "🍷": "Wine Glass",
-    "🍸": "Cocktail Glass",
-    "🍹": "Tropical Drink",
-    "🍺": "Beer Mug",
-    "🍻": "Clinking Beer Mugs",
-    "🥂": "Clinking Glasses",
-    "🥃": "Tumbler Glass",
-    "🥤": "Cup with Straw"
-  };
+var emojiDictionary = {
+  "🍇": "Grapes",
+  "🍈": "Melon",
+  "🍉": "Watermelon",
+  "🍊": "Tangerine",
+  "🍋": "Lemon",
+  "🍌": "Banana",
+  "🍍": "Pineapple",
+  "🥭": "Mango",
+  "🍎": "Red Apple",
+  "🍐": "Pear",
+  "🍒": "Cherries",
+  "🍅": "Tomato",
+  "🍆": "Eggplant",
+  "🥕": "Carrot",
+  "🥦": "Broccoli",
+  "🍞": "Bread",
+  "🍗": "Poultry Leg",
+  "🥩": "Cut of Meat",
+  "🥓": "Bacon",
+  "🍔": "Hamburger",
+  "🍟": "French Fries",
+  "🍕": "Pizza",
+  "🌭": "Hot Dog",
+  "🥪": "Sandwich",
+  "🌮": "Taco",
+  "🥚": "Egg",
+  "🍿": "Popcorn",
+  "🧈": "Butter",
+  "🍨": "Ice Cream",
+  "🍩": "Doughnut",
+  "🍪": "Cookie",
+  "🎂": "Birthday Cake",
+  "🍰": "Shortcake",
+  "🧁": "Cupcake",
+  "🥧": "Pie",
+  "🍫": "Chocolate Bar",
+  "🍬": "Candy",
+  "🍭": "Lollipop",
+  "🍶": "Sake",
+  "🍾": "Bottle with Popping Cork",
+  "🍷": "Wine Glass",
+  "🍸": "Cocktail Glass",
+  "🍹": "Tropical Drink",
+  "🍺": "Beer Mug",
+  "🍻": "Clinking Beer Mugs",
+  "🥂": "Clinking Glasses",
+  "🥃": "Tumbler Glass",
+  "🥤": "Cup with Straw"
+};
 
+export default function App() {
   var [explain, setexplain] = useState("");
 
   const inputText = (e) => {
     var userInput = e.target.value;
-    if (userInput != null) {
+    if (userInput) {
       if (userInput in emojiDictionary) {
         setexplain(emojiDictionary[userInput]);
       } else {
@@ -67,12 +67,18 @@ export default function App() {
       setexplain("Please enter some value");
     }
   };
-
+  var list = Object.keys(emojiDictionary);
   return (
     <div className="App">
       <h1 style={{ color: "blue" }}>Welcome </h1>
       <input onChange={inputText} />
       <h3>Define: {explain}</h3>
+      <hr />
+      <ul>
+        {list.map(function (element, index, array) {
+          return <li> {array[index]} </li>;
+        })}
+      </ul>
     </div>
   );
 }
